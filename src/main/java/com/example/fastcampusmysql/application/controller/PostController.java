d92026b6cc8cde044561ec40ac0e951920ebe277
H-JWANNA <hongjjwan@gmail.com>
2023-04-27 22:58:37 +0900

    feat: 게시글 API 생성

---
+++ src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
@@ -0,0 +1,38 @@
+package com.example.fastcampusmysql.application.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.domain.post.dto.PostCommand;
+import com.example.fastcampusmysql.domain.post.dto.PostDto;
+import com.example.fastcampusmysql.domain.post.service.PostReadService;
+import com.example.fastcampusmysql.domain.post.service.PostWriteService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+	private final PostWriteService postWriteService;
+	private final PostReadService postReadService;
+
+	@PostMapping
+	public PostDto create(PostCommand command) {
+		// TODO: 회원 유효성 검증
+		// TODO: 이미지 업로드, 비디오 업로드, 게시물 수정, 댓글 등 부가 기능 추후에 추가
+		return postWriteService.create(command);
+	}
+
+	@GetMapping("/daily-post-counts")
+	public List<DailyPostCount> getDailyPostCounts(DailyPostCountRequest request) {
+
+		return postReadService.getDailyPostCount(request);
+	}
+}
